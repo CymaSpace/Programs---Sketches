abe34ed88e3019cd6580c0d03aeae6743fe8f4d5
JoshMarthaller <joshmarthaller@gmail.com>
2014-04-30 14:55:14 -0700

    updated version of file
    ...

--- Josh Marthaller/MSGEQ7_Top_Frequency_Propogate/MSGEQ7_Top_Frequency_Propogate.ino
+++ Josh Marthaller/MSGEQ7_Top_Frequency_Propogate/MSGEQ7_Top_Frequency_Propogate.ino
@@ -1,19 +1,29 @@
 //code from https://github.com/jdbugman/anduino-neopixel/blob/master/soundNeo/soundNeo.ino
 #include <Adafruit_NeoPixel.h>
-#define PIN 6
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(40, PIN, NEO_GRB + NEO_KHZ800); //first number controls the amount of pixels you have (add 4 so the drip falls off the edge)
- 
-int led_strip_length = 40;    //select 1 thru 10
- 
-int strip_length = 20;
-int prop_left[20];
-int prop_left_his[20];
-int prop_right[20];
-int prop_right_his[20];
+#define PIN 5
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, PIN, NEO_GRB + NEO_KHZ800); //first number controls the amount of pixels you have (add 4 so the drip falls off the edge)
+
+//length of LED strip 
+int led_strip_length = 150;    //select 1 thru 10
+
+//half the srip length (length on each side) 
+int strip_length = 75;
+int prop_left[75];
+int prop_left_his[75];
+int prop_right[75];
+int prop_right_his[75];
+
+//if # is 10, it updates every 10th iteration, thus higher number makes
+//refresh rate slower
 int refresh = 10; //higher number refreshes slow
 int refresh_counter = 0;
+
 int readingsTotal = 800;
 int readingsChanged = 100;
+
+//was for the dial
+int analogPinpot = 2;
+int minFilter = 100;
  
 //values for MSGEQ7 Chip
 int analogPinL = 1; // read from multiplexer using analog input 0
@@ -27,6 +37,7 @@ int previousSpectrumValueR[6]; // to hold a2d values
 int global_brightness = 96; // Sets global brightness, i.e. 64 is 1/4 brightness.
 int useColor[2];
 int num;
+int middle_pin = 25;
  
  
 void setup() {
@@ -40,6 +51,8 @@ void setup() {
   analogReference(DEFAULT);
   digitalWrite(resetPin, LOW);
   digitalWrite(strobePin, HIGH);
+  
+  pinMode(analogPinpot, INPUT);
 }
  
 void loop() {    
@@ -56,6 +69,23 @@ void sevenWaves(){
   int changePinL = 0;
   int changePinR = 0;
   int k,i,r,g,b;
+  int pot;
+  int tmp_adj = 0;
+  
+  
+  
+  //un comment below if the pentomiter is hooked up
+  //these will override the default setting if used
+
+  //--fetch pentimoter value 
+  //pot = analogRead(analogPinpot);
+  
+  //Serial.println(pot);
+  //--for the min amplitude to count as a reading
+  //minFilter = round(pot * .4);
+  
+  //--refresh rate
+  //refresh = round(pot * .05);
  
   //get readings from chip
   for (i = 0; i < 7; i++)
@@ -67,12 +97,17 @@ void sevenWaves(){
     spectrumValueL[i] = analogRead(analogPinL);
     spectrumValueR[i] = analogRead(analogPinR);
  
+   
+   
     //figure out which pin changed the most since last reading
     if( abs(previousSpectrumValueL[i]-spectrumValueL[i]) > changeL)
     {
         changeL = abs(previousSpectrumValueL[i]-spectrumValueL[i]);
-        if( spectrumValueL[i] > 100  )
+        if( spectrumValueL[i] > minFilter  )
           changePinL = (i+1);
+          
+        if( spectrumValueL[i] > 400 && i <=1)
+          tmp_adj+= 23;
          
          readingsChanged++;
     }
@@ -81,8 +116,11 @@ void sevenWaves(){
     if( abs(previousSpectrumValueR[i]-spectrumValueR[i]) > changeR)
     {
         changeR = abs(previousSpectrumValueR[i]-spectrumValueR[i]);
-        if( spectrumValueR[i] > 100 )
+        if( spectrumValueR[i] > minFilter )
           changePinR = (i+1);
+          
+        if( spectrumValueR[i] > 400 && i <=1)
+          tmp_adj+= 23;
          
          readingsChanged++;
     }
@@ -90,14 +128,24 @@ void sevenWaves(){
   }
  
   //refresh = round(readingsTotal/readingsChanged);
-  int tmp_adj = 0;
-  tmp_adj = round(.05 * readingsChanged);
+  
+ 
+  
+  if(tmp_adj < 0) { tmp_adj = 0; }
+  if(tmp_adj > 200) { tmp_adj = 200; }
+
+  
+  
   refresh_counter++;
-  if(refresh_counter==refresh)
+  if(refresh_counter>=(refresh - round(tmp_adj * .1)))
+  //if(refresh_counter>=refresh )
   {
+    
+
     refresh_counter = 0;
     //refresh =  round(5 * (readingsChanged / (readingsChanged * 7 * 2)));
     readingsChanged = 0;
+    tmp_adj-= 1;
  
   //current;
   prop_right[0] = changePinR;
@@ -159,7 +207,7 @@ void sevenWaves(){
  
  
  
-  //strip.setBrightness(global_brightness);
+  strip.setBrightness(global_brightness);
   strip.show();
  
 }
@@ -205,4 +253,5 @@ void get_color()
     useColor[0] = r;
     useColor[1] = g;
     useColor[2] = b;
+    
 }
