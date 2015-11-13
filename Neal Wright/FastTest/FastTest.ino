a5971433002dfb7346f348a48586177059d4e31d
Neal Wright <Neal@Hendeca.com>
2015-11-12 17:10:17 -0800

    Created the A4 sketch from the updated FastTest sketch.

--- Neal Wright/FastTest/FastTest.ino
+++ Neal Wright/FastTest/FastTest.ino
@@ -2,7 +2,7 @@
 #include "EEPROM.h"
 
 /* Output pin definitions */
-#define NUM_LEDS 151 // Number of LED's in the strip
+#define NUM_LEDS 71 // Number of LED's in the strip
 #define DATA_PIN 6 // Data out
 #define ANALOG_PIN_L 1 // Left audio channel
 #define ANALOG_PIN_R 0 // Right audio channel
@@ -20,17 +20,18 @@
 #define RIGHT_START_POINT ((NUM_LEDS / 2) + 1) // Starting LED for the right side
 #define RIGHT_END_POINT (NUM_LEDS -1) // Generally the end of the right side is the last LED
 #define LED_STACK_SIZE (NUM_LEDS / 2) // How many LED's in each stack
-#define MAX_AMPLITUDE 5000 // Maximum possible amplitude value
+#define MAX_AMPLITUDE 4700 // Maximum possible amplitude value
+#define MAX_AMPLITUDE_MULTIPLIER 380
 #define MIN_AMPLITUDE 420 // Lowest possible amplitude value (Higher number causes there to be more blank LED's)
-#define MAX_SENSITIVITY 4530 // The maximum possible sensitivity value
-#define MIN_SENSITIVITY 0 // The minimum possible sensitivity value
-#define SENSITIVITY_MULTIPLIER 300 // Higher = range of sensitivity values on pot is lower
+#define MIN_AMPLITUDE_MULTIPLIER 200
+#define SENSITIVITY_MULTIPLIER 200 // Higher = range of sensitivity values on pot is lower
 
 int monomode; // Used to duplicate the left single for manual input
 int refresh_rate; // Refresh rate of the animation
 int refresh_counter = 0; // Looping variable for refresh loop
 int sensitivity; // Sensitivity value
-int min_amplitude;
+int max_amplitude;
+int start_hue = 0;
 
 /* Represents the left and right LED color values.
  * In the case of an odd number of LED's, you need to adjust these
@@ -45,7 +46,6 @@ CHSV color(0, 255, 255);
 CRGB leds[NUM_LEDS]; // Represents LED strip
 
 void setup() {
-  Serial.begin(9600); // print to serial monitor
 
   // Instantiate Neopixels with FastLED
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
@@ -91,6 +91,8 @@ void loop() {
   
   // Change color mode if stomp button is pressed
   if(stomp_pressed()) {
+    change_color_mode();
+    delay(100); // Keeps the pedal from switching colors too quickly
   }
 
   // Get the sum of the amplitudes of all 7 frequency bands
@@ -106,9 +108,7 @@ void loop() {
    *  set it to the max or min respectively.
    */
   sensitivity = (analogRead(SENSITIVITY_POT_PIN) / SENSITIVITY_DIVISOR) * SENSITIVITY_MULTIPLIER;
-  min_amplitude = ((analogRead(SENSITIVITY_POT_PIN) / SENSITIVITY_DIVISOR) * 200) + 420;
-  if(sensitivity > MAX_SENSITIVITY) sensitivity = MAX_SENSITIVITY;
-  if(sensitivity < MIN_SENSITIVITY) sensitivity = MIN_SENSITIVITY;
+  max_amplitude = MAX_AMPLITUDE - (((1023 - analogRead(SENSITIVITY_POT_PIN)) / SENSITIVITY_DIVISOR) * MAX_AMPLITUDE_MULTIPLIER);
 
   // REFRESH_DIVISOR lowers the range of possible pot values
   refresh_rate = analogRead(REFRESH_POT_PIN) / REFRESH_DIVISOR;
@@ -166,9 +166,11 @@ int stomp_pressed() {
   }
 }
 
-// Change which color values correspond to amplitude values
 void change_color_mode() {
-  // @TO-DO: Finish this function
+  start_hue += 85;
+  if(start_hue >= 255) {
+    start_hue = start_hue - 255;
+  }
 }
 
 // Read in and sum amplitudes for the 7 frequency bands
@@ -194,20 +196,32 @@ int get_freq_sum(int pin) {
   return spectrum_total;
 }
 
-// Sets led 'position' to 'value' and converts the value to an HSV value
+/* Sets led 'position' to 'value' and converts the value to an HSV value.
+ * Compared to the A3 code, this code produces color values closer to white.
+ * The A3 code always has only two colors lit at a time. For example red and 
+ * green but not blue or blue and red but not green.
+ * As a result, the colors are more like pastel hues than bold hues.
+ * Another option would be to do something similar to the limited RGB values from A3.
+ */
 void set_LED_color(int position, int value) {
-  if(value < min_amplitude) {
-    value = 0;
+  // If lower than min amplitude, set to min amplitude
+  if(value <= MIN_AMPLITUDE) {
+    value = MIN_AMPLITUDE;
   }
+  
+  // Subtract min amplitude so lowest value is 0
   value -= MIN_AMPLITUDE;
-  float max = (MAX_AMPLITUDE - MIN_AMPLITUDE);
-  float ratio = ((float)value / max);
+  
+  float max = (max_amplitude - MIN_AMPLITUDE);
+  if(value > max) value = max;
+  float ratio = ((float)(value / max));
   if(ratio == 0) {
     color.val = 0;
   } else {
     color.val = 255;
   }
-  color.hue = 255 - (ratio * 255);
+  color.saturation = 255;
+  color.hue = start_hue + (ratio * 255);
   leds[position] = color;
 }
 
@@ -222,4 +236,3 @@ void push_stack(int stack[], int value) {
   }
   stack[0] = value;
 }
-
