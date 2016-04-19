b6d7f272ed3b258742faffffedd7b0ec382c9e72
Tim Gorbunov <tgor152@gmail.com>
2016-04-18 17:28:31 -0700

    added piano updates

--- Tim Gorbunov/audiolux_master/audiolux.h
+++ Tim Gorbunov/audiolux_master/audiolux.h
@@ -1,51 +1,22 @@
 /*
-  Morse.h - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
+  audiolux.h - Library 
+  Created by Tim Gorbunov
+  Released
 */
 #ifndef audiolux_h
 #define audiolux_h
 
+
 #include "Arduino.h"
 #include <FastLED.h>
-#include "EEPROM.h"
-
-#define ANALOG_PIN_L 1 // Left audio channel
-#define ANALOG_PIN_R 0 // Right audio channel
-#define REFRESH_POT_PIN 2 // Left pot
-#define SENSITIVITY_POT_PIN 3 // Right pot
-#define STOMP_PIN 5 // The pin connected to the stomp button
-
-/* Sensitivity variables, refresh variables, and start/end points */
-#define REFRESH_DIVISOR 80. // Higher = range of refresh values is lower
-#define SENSITIVITY_DIVISOR 100. // Higher = range of sensitivity values on pot is lower
-#define MAX_AMPLITUDE 4700 // Maximum possible amplitude value
-#define MAX_AMPLITUDE_MULTIPLIER 380
-#define MIN_AMPLITUDE 545 // Lowest possible amplitude value (Higher number causes there to be more blank LED's)
-#define MIN_AMPLITUDE_MULTIPLIER 200
-#define SENSITIVITY_MULTIPLIER 200 // Higher = range of sensitivity values on pot is lower
-
 
 class Audiolux
 {
   public:
-    Audiolux(uint8_t DATA_PIN, uint8_t NUM_LEDS);
-    void dot();
-    void dash();
-    uint8_t
-      STROBE_PIN = 12,
-      RESET_PIN = 13;
-      
+    Audiolux();
+    void runs();
   private:
-    uint8_t 
-      _DATA_PIN;
-    uint16_t 
-      _NUM_LEDS,
-      _LEFT_START_POINT,
-      _LEFT_END_POINT,
-      _RIGHT_START_POINT,
-      _RIGHT_END_POINT,
-      _LED_STACK_SIZE;
+    CRGB leds[100];
     
 };
 
--- Tim Gorbunov/audiolux_master/audiolux_master.ino
+++ Tim Gorbunov/audiolux_master/audiolux_master.ino
@@ -1,11 +1,55 @@
 #include "audiolux.h"
+#include <FastLED.h>
 
-//Audiolux "outputname"(PIN, NUM_LED)
-Audiolux al1(6, 100);//initialize new audiolux output on this pin
+Audiolux al1;
+
+void setup() {
+}
+void loop() {
+  al1.runs();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////
+//IDEAL PROGRAM LAYOUT
+////////////////////////////////////////////////////////////////////
+/*
+
+#include "audiolux.h"
+
+Audiolux show();
+
+show.setAudio(MSGEQ7, STROBE_PIN, RESSET, PIN);
+show.setAnimation(PIN, A4, SHOWSIZE);
+show.setLED(NUM_DIMENTIONS, NUM_PIXELS);
 
 void setup() {
 }
 
 void loop() {
-  al1.dot();
+  show.updateAnimation();
+  show.display();
 }
+
+
+
+//*/
